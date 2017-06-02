d648643f349c318fe1760b2da580a9d4a3c64dc0
Alexandre Abadie <alexandre.abadie@inria.fr>
2017-06-02 17:16:14 +0200

    examples: reduce number of jobs

--- examples/joblib_yarn_parallel.py
+++ examples/joblib_yarn_parallel.py
@@ -7,6 +7,6 @@ from joblibhadoop.yarn import YarnBackend
 
 if __name__ == '__main__':
     register_parallel_backend('yarn', YarnBackend)
-    with parallel_backend('yarn', n_jobs=10):
-        Parallel(n_jobs=10, verbose=100)(
+    with parallel_backend('yarn', n_jobs=1):
+        Parallel(verbose=100)(
             delayed(sqrt)(i**2) for i in range(100))
