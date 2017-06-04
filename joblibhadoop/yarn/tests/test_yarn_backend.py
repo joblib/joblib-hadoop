30ff92b11334777cfef3284b471b37ca3940170a
Alexandre Abadie <alexandre.abadie@inria.fr>
2017-06-04 23:36:10 +0200

    yarn/tests: fix njobs=-1 case

--- joblibhadoop/yarn/tests/test_yarn_backend.py
+++ joblibhadoop/yarn/tests/test_yarn_backend.py
@@ -18,16 +18,16 @@ skip_localhost = pytest.mark.skipif(__namenode__ == 'localhost',
 
 
 @skip_localhost
-def test_simple_parallel_backend():
+def test_parallel_no_njobs_raises_valueerror():
     """Check that calling parallel with Yarn backend works."""
     register_parallel_backend('yarn', YarnBackend)
 
     # Run in parallel using Yarn backend
-    with parallel_backend('yarn'):
-        result = Parallel(verbose=100)(
-            delayed(sqrt)(i**2) for i in range(100))
-
-    assert [sqrt(x**2) for x in range(100)] == result
+    with pytest.raises(ValueError) as excinfo:
+        with parallel_backend('yarn'):
+            result = Parallel(verbose=100)(
+                delayed(sqrt)(i**2) for i in range(100))
+    assert 'n_jobs < 0 is not implemented yet' in str(excinfo.value)
 
 
 @skip_localhost
